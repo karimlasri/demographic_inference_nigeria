4b9998d4cd385ff651345fc98c8c19632eb0297a
Karim Lasri <karimlasri@hotmail.com>
2024-09-19 14:21:13 +0200

    Refactoring name matching

--- README.md
+++ README.md
@@ -3,14 +3,18 @@ This repository contains the implementation of [Large-Scale Demographic Inferenc
 
 # Data Format
 ## User profiles
-
+User profiles can be stored as a csv or parquet file and should contain at least a column with users' twitter ID as `user_id`, one with the users' names as `name`, and one with the users' screen name as `screen_name`. It could be for example stored as `data/user_profiles.csv` along with other data files. 
 ## Followership data
 
 
 # Usage
 First perform name matching by running performing the following command :
 ```
-python3 models/match_names.py <path_to_user_profiles>
+python3 models/match_names.py --user_profiles <path_to_user_profiles> --out_path <output_path>
+```
+For example
+```
+python3 models/match_names.py --user_profiles data/user_profiles.csv --out_path data/all_users_with_demographics.csv
 ```
 This will save scores from name matching for each user in the predictions folder.
 
--- models/match_names.py
+++ models/match_names.py
@@ -23,10 +23,12 @@ CLASSES = {
     'religion':['christian', 'muslim']
          }
 
-    
-def match_name(x, names_list):
+
+def match_name(user_name, names_list):
+    """ Match a given name (user name or screen name) against a list of names.
+    A name will be matched if it is a substring of the input user name. """
     regex = re.compile(fr"\b(?:{'|'.join(names_list)})",re.IGNORECASE)
-    match_list = [match.group(0) for match in regex.finditer(x)]
+    match_list = [match.group(0) for match in regex.finditer(user_name)]
     if len(match_list) > 0:
         return match_list
     else:
@@ -40,6 +42,7 @@ def match_names(user_profiles, names_list):
 
 
 def predict(attr, names_mapping, user_profiles):
+    """ Predict a demographic attribute for given users based on their matched name and screen name, and the name mapping to that attribute """
     predictions = list()
     for i in range(user_profiles.shape[0]):
         x = user_profiles['matched_name'][i] + user_profiles['matched_screen_name'][i]
@@ -84,7 +87,7 @@ def predict(attr, names_mapping, user_profiles):
     return predictions
 
 
-def score_for_name(name, names_mapping, attr):
+def score_for_name(name, names_mapping, attr, gender_df):
     scores = {l:0 for l in CLASSES[attr]}
     y = names_mapping[attr][name]
     if not pd.isnull(y):
@@ -123,7 +126,7 @@ def merge_scores(all_scores, weights, attr):
     return merged_scores
 
 
-def score(attr, names_mapping, df):
+def score(attr, names_mapping, df, gender_df):
     """ Compute name matching scores for all target classes of a given demographic attribute (e.g. ethnicity), and for 
     all observations of a given df """
     scores = {l:[] for l in CLASSES[attr]}
@@ -131,7 +134,7 @@ def score(attr, names_mapping, df):
         x =  df['matched_name'].iloc[i] + df['matched_screen_name'].iloc[i]
         x = list(set(x)) # We keep only one instance of each name
 
-        all_scores = [score_for_name(unidecode(n.lower()), names_mapping, attr) for n in x]
+        all_scores = [score_for_name(unidecode(n.lower()), names_mapping, attr, gender_df) for n in x]
         # Compute a score vector for each name (e.g. if not ambiguous, 1 for target label, if ambiguous such as
         # muslim/christian, 0.5 for each), results in a vector list
         # Example : if 'Anu' is 'Yoruba' and 'Ife' is 'Yoruba/Igbo', returns [(0,1,0), (0,0.5,0.5)]
@@ -144,7 +147,7 @@ def score(attr, names_mapping, df):
         
         merged = merge_scores(all_scores, weights, attr)
         # The merged vector is the mean of obtained vectors in "all_scores" variable, weighted by each name's length.
-        # Example : with 'Karim' and 'Manu', sumlen=9, and weights=[5/9, 4/9] (the longer a name, the more reliable it is)
+        # Example : with 'Abba' and 'Aashif', sumlen=10, and weights=[4/10, 6/10] (the longer a name, the more reliable it is)
         
         for l in scores:
             scores[l].append(merged[l])
@@ -155,61 +158,68 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser(
         description="Assign name-based demographic attributes by matching names of twitter users using a database mapping names to demographic attributes."
     )
-
+    
     parser.add_argument("--user_profiles", type=str, help="Path to the user profiles.")
-
+    parser.add_argument("--out_path", type=str, help="Path where user profiles with their name-based scores for each attribute will be saved.")
+    
     args = parser.parse_args()
-
+    
     profiles_path = args.user_profiles # Path to folder containing list of parquet files with user profiles
-
+    
     # Load and preprocess labeled list of names
     names_mapping = load_name_mapping(NAMES_TO_ATTRIBUTES_MAP)
-    
-    # Load and preprocess labeled test users list
-    labeled_profiles = pd.read_csv(ANNOTATIONS_PATH)
-    labeled_profiles = labeled_profiles.dropna()
-
-    labeled_profiles['ethnicity'] = labeled_profiles['ethnicity'].apply(str.lower)
-    labeled_profiles['religion'] = labeled_profiles['religion'].apply(str.lower)
-    labeled_profiles['gender'] = labeled_profiles['gender'].apply(str.lower)
-
-    labeled_profiles = labeled_profiles.loc[labeled_profiles['gender'].isin(['m', 'f'])]
-    labeled_profiles = labeled_profiles.loc[labeled_profiles['ethnicity'].isin(['igbo', 'yoruba', 'hausa'])]
-    labeled_profiles = labeled_profiles.loc[labeled_profiles['religion']!='unsure']
-    
-    n_samples = labeled_profiles.shape[0]
-    labeled_profiles = labeled_profiles.reset_index(drop=True)
     
     # Loading matching utils
     name_list = names_mapping.index.tolist()
     name_list.sort(key=len, reverse=True)
     name_list = [name for name in name_list if len(name)>2]
-    
-    labeled_profiles['matched_screen_name'] = labeled_profiles['screen_name'].apply(lambda x: match_name(x,name_list))
-    labeled_profiles['matched_name'] = labeled_profiles['name'].apply(lambda x: match_name(x,name_list))
-    
+
+    # Load additional scraped gender mapping    
     gender_df = pd.read_csv('data/names_gender_scraped.csv')
     gender_df['Forename'] = gender_df['Forename'].str.lower()
     gender_df = gender_df.set_index('Forename').drop('Unnamed: 0', axis=1)
     
-    # Get scores for test_df
-    scores_test = score('ethnicity', names_mapping, labeled_profiles)
-    
-    df = pd.read_parquet(profiles_path)
-    df['matched_screen_name'] = df['user_screen_name'].apply(lambda x: match_name(x,name_list))
-    df['matched_name'] = df['user_name'].apply(lambda x: match_name(x,name_list))    
-    df = df.reset_index(drop=True)
+    ## Name matching for unlabeled user profiles
+    # Load user profiles    
+    user_profiles = pd.read_parquet(profiles_path)
+    user_profiles = match_names(user_profiles, names_list)
+    user_profiles = user_profiles.reset_index(drop=True)
     
     # Score each demographic attribute and save file
     if not os.path.exists(OUTPUT_PATH):
         os.mkdir(OUTPUT_PATH)
     for attr in ('ethnicity', 'religion', 'gender'):
-        attr_scores = score(attr, names_mapping, df)
-        attr_scores['user_id'] = df['user_id']
+        attr_scores = score(attr, names_mapping, user_profiles, gender_df)
+        attr_scores['user_id'] = user_profiles['user_id']
         attr_scores.to_csv(f'{OUTPUT_PATH}/name_matching_scores_{attr}.csv')
         
         attr_classes = CLASSES[attr]
         predictions = attr_scores[attr_classes].apply(lambda x : attr_classes[np.argmax(x.values)], axis=1)
 
-        df[f'{attr}_name_predict'] = predictions
-    df.to_csv('data/all_users_with_demographics.csv')
+        user_profiles[f'{attr}_name_predict'] = predictions
+        
+    out_path = args.out_path
+    user_profiles.to_csv('data/all_users_with_demographics.csv')
+
+    
+    ## Evaluate name matching on test set
+    # Load and preprocess labeled test users list
+    labeled_profiles = pd.read_csv(ANNOTATIONS_PATH)
+    labeled_profiles = labeled_profiles.dropna()
+    
+    labeled_profiles['ethnicity'] = labeled_profiles['ethnicity'].apply(str.lower)
+    labeled_profiles['religion'] = labeled_profiles['religion'].apply(str.lower)
+    labeled_profiles['gender'] = labeled_profiles['gender'].apply(str.lower)
+    
+    labeled_profiles = labeled_profiles.loc[labeled_profiles['gender'].isin(['m', 'f'])]
+    labeled_profiles = labeled_profiles.loc[labeled_profiles['ethnicity'].isin(['igbo', 'yoruba', 'hausa'])]
+    labeled_profiles = labeled_profiles.loc[labeled_profiles['religion']!='unsure']
+    
+    n_labeled_profiles = labeled_profiles.shape[0]
+    labeled_profiles = labeled_profiles.reset_index(drop=True)
+
+    # Match names for test set
+    labeled_profiles = match_names(labeled_profiles, names_list)
+
+    # Get scores for test_df
+    scores_test = score('ethnicity', names_mapping, labeled_profiles, gender_df)
